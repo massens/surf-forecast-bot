75ca67d8b67337e8c7d5f6b08b8e96388b4c55ec
Alfons Figuls Bonache <alfonsfiguls@gmail.com>
2016-12-05 14:55:46 +0100

    Hem mogut la carpeta bot a la carpeta app

--- app/bot/telegram-example.rb
+++ app/bot/telegram-example.rb
@@ -11,9 +11,6 @@ Telegram::Bot::Client.run(token) do |bot|
       bot.api.send_message(chat_id: message.chat.id, text: "Ahoy, #{message.from.first_name}!!")
     when 'Bye'
       bot.api.send_message(chat_id: message.chat.id, text: "See ya in the line up, #{message.from.first_name}!!")
-    	
-    when 'Are there any waves?'
-    	bot.api.send_message(chat_id: message.chat.id, text: "Mmmm... Nope, flat :(")
     end
   end
 end
